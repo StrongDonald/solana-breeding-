fa70dffe037b59eb5b22eef2063da11ef408062f
StrongDonald <topdev19991228@gmail.com>
2022-04-06 06:20:01 -0700

    Add tokenaccount

--- programs/arcryptiannft-breeding-solana/src/contexts.rs
+++ programs/arcryptiannft-breeding-solana/src/contexts.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::TokenAccount;
+use std::ops::Deref;
 
 #[derive(Accounts)]
 pub struct InitializeBreeding<'info> {
@@ -8,24 +9,54 @@ pub struct InitializeBreeding<'info> {
         seeds = [authority.key().as_ref()],
         bump,
         payer = authority,
-        space = 320,
+        space = Breeding::space(),
     )]
-    breeding: Account<'info, Breeding>,
+    pub breeding: Account<'info, Breeding>,
+
+    #[account(
+        mut,
+        seeds = [b"male"],
+        bump,
+        constraint = male_lock_account
+         .clone().into_inner().deref().owner == authority.key(),
+    )]
+    pub male_lock_account: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        seeds = [b"female"],
+        bump,
+        constraint = female_lock_account
+         .clone().into_inner().deref().owner == authority.key(),
+    )]
+    pub female_lock_account: Account<'info, TokenAccount>,
+
 
     #[account(mut)]
     pub authority: Signer<'info>,
 
     pub system_program: Program<'info, System>,
+
+    pub clock: Sysvar<'info,Clock>,
 }
 
 #[account]
 pub struct Breeding {
-    timestamp: String,
-    is_breeding: bool,
+    // space 8 + 1
+    pub authority: Pubkey,
+    pub timestamp: u64,     // 8
+    pub is_breeding: bool,  // 1
 }
 
-// impl Breeding {
-//     fn len(&self) -> usize {
-//         let len = 
-//     }
-// }
+impl Breeding {
+    fn space() -> usize {
+        // discriminator
+        8 +
+        // Pubkey
+        32 +
+        // u64
+        8 +
+        // bool
+        1     
+    }
+}
--- programs/arcryptiannft-breeding-solana/src/lib.rs
+++ programs/arcryptiannft-breeding-solana/src/lib.rs
@@ -9,7 +9,12 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod arcryptiannft_breeding_solana {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<InitializeBreeding>) -> Result<()> {
+        let breeding = &mut ctx.accounts.breeding;
+        breeding.authority= ctx.accounts.authority.key().clone();
+        breeding.timestamp = ctx.accounts.clock.unix_timestamp as u64;
+        breeding.is_breeding = false;
+        
         Ok(())
     }
 }
